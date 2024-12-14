2e1ac6bdf443d3f515fb290c2a1c1fbfd4bd3597
JV <jvvlcek@gmail.com>
2024-12-14 13:27:00 +0100

    Fix add fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,7 @@ import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({weight:"400"});
+export const lusitana = Lusitana({
+  subsets: ['latin'], // Add this line to specify the subset
+  weight: ['400', '700'],  // Add weights if needed
+});
